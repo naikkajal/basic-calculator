ffc30e0ce3fbc954cff44e5b5c0703c832f2bf26
Kajal Naik <naikkajal0603@gmail.com>
2024-04-12 23:34:23 +0530

    completed

--- lib/calculator_view.dart
+++ lib/calculator_view.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 
 class CalculatorView extends StatelessWidget {
-  const CalculatorView({super.key});
+  const CalculatorView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children:[
-         TextField(
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.white
-                ),
-              ),
-              hintText: "Enter a number"
-            ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 32.0),
+      child: Column(
+        children: [
+          Display1(hint: "Enter First Number"), 
+          SizedBox(height: 30,),
+          Display1(hint: "Enter Second Number"), 
+        ],
+      ),
+    );
+  }
+}
+
+class Display1 extends StatelessWidget {
+  final String hint; 
+
+  const Display1({Key? key, required this.hint}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
           ),
-        
-        
-      ],
+        ),
+        hintText: hint, // Use the custom hint text passed to the widget
+      ),
     );
   }
 }
