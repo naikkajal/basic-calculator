38f455c9f0c355a6909fbb23514678d85df2ac0b
Kajal Naik <naikkajal0603@gmail.com>
2024-04-13 22:43:36 +0530

    completed

--- lib/calculator_view.dart
+++ lib/calculator_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CalculatorView extends StatelessWidget {
@@ -6,12 +7,42 @@ class CalculatorView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 32.0),
+      padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 32.0),
       child: Column(
         children: [
           Display1(hint: "Enter First Number"), 
           SizedBox(height: 30,),
-          Display1(hint: "Enter Second Number"), 
+          Display1(hint: "Enter Second Number"),
+          SizedBox(height: 80,),
+          Text("0",
+          style: TextStyle(
+            fontSize: 60.0,
+            fontWeight: FontWeight.bold
+          ),), 
+          Spacer(),
+          Padding(
+            padding: EdgeInsets.only(bottom: 60),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(onPressed: () {},
+                child: const Icon(Icons.add),
+                backgroundColor: Colors.orange),
+                FloatingActionButton(onPressed: () {},
+                backgroundColor: Colors.orange,
+                child: const Icon(CupertinoIcons.minus),),
+                FloatingActionButton(onPressed: () {},
+                child: const Icon(CupertinoIcons.multiply),
+                backgroundColor: Colors.orange),
+                FloatingActionButton(onPressed: () {},
+                child: const Icon(CupertinoIcons.divide),
+                backgroundColor: Colors.orange),
+                FloatingActionButton(onPressed: () {},
+                child: const Icon(CupertinoIcons.delete),
+                backgroundColor: Colors.orange)
+              ],
+            ),
+          )
         ],
       ),
     );
