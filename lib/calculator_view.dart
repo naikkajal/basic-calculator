1794c5fee3c97495967df0c160987d707620038a
Kajal Naik <naikkajal0603@gmail.com>
2024-04-15 23:52:36 +0530

    changes

--- lib/calculator_view.dart
+++ lib/calculator_view.dart
@@ -1,45 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorView extends StatelessWidget {
+class CalculatorView extends StatefulWidget {
   const CalculatorView({Key? key}) : super(key: key);
 
+  @override
+  State<CalculatorView> createState() => _CalculatorViewState();
+}
+
+class _CalculatorViewState extends State<CalculatorView> {
+  int x = 0;
+  int y = 0;
+  int z = 0;
+
+  final displayOneController = TextEditingController();
+  final displayTwoController= TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 32.0),
       child: Column(
         children: [
-          Display1(hint: "Enter First Number"), 
-          SizedBox(height: 30,),
-          Display1(hint: "Enter Second Number"),
-          SizedBox(height: 80,),
-          Text("0",
-          style: TextStyle(
-            fontSize: 60.0,
-            fontWeight: FontWeight.bold
-          ),), 
+          DisplayOne(hint: "Enter First Number", controller:displayOneController),
+          SizedBox(
+            height: 30,
+          ),
+          DisplayOne(hint: "Enter Second Number", controller: displayTwoController),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            z.toString(),
+            style: TextStyle(fontSize: 60.0, fontWeight: FontWeight.bold),
+          ),
           Spacer(),
           Padding(
             padding: EdgeInsets.only(bottom: 60),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                FloatingActionButton(onPressed: () {},
-                child: const Icon(Icons.add),
-                backgroundColor: Colors.orange),
-                FloatingActionButton(onPressed: () {},
-                backgroundColor: Colors.orange,
-                child: const Icon(CupertinoIcons.minus),),
-                FloatingActionButton(onPressed: () {},
-                child: const Icon(CupertinoIcons.multiply),
-                backgroundColor: Colors.orange),
-                FloatingActionButton(onPressed: () {},
-                child: const Icon(CupertinoIcons.divide),
-                backgroundColor: Colors.orange),
-                FloatingActionButton(onPressed: () {},
-                child: const Icon(CupertinoIcons.delete),
-                backgroundColor: Colors.orange)
+                FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.add),
+                    backgroundColor: Colors.orange),
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Colors.orange,
+                  child: const Icon(CupertinoIcons.minus),
+                ),
+                FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(CupertinoIcons.multiply),
+                    backgroundColor: Colors.orange),
+                FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(CupertinoIcons.divide),
+                    backgroundColor: Colors.orange),
+                FloatingActionButton(
+                    onPressed: () {},
+                    child: const Icon(CupertinoIcons.delete),
+                    backgroundColor: Colors.orange)
               ],
             ),
           )
@@ -49,21 +69,23 @@ class CalculatorView extends StatelessWidget {
   }
 }
 
-class Display1 extends StatelessWidget {
-  final String hint; 
+class DisplayOne extends StatelessWidget {
+  final String hint;
+  final TextEditingController? controller; 
 
-  const Display1({Key? key, required this.hint}) : super(key: key);
+  const DisplayOne({Key? key, required this.hint, this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller, 
       decoration: InputDecoration(
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(
             color: Colors.black,
           ),
         ),
-        hintText: hint, // Use the custom hint text passed to the widget
+        hintText: hint, 
       ),
     );
   }
